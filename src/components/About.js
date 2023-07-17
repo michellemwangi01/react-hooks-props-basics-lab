e4fa4506f7e9a1cbd05a6bed5096cc8df3d6ec35
michellemwangi01 <michellemwangim@gmail.com>
2023-07-17 20:52:49 +0300

    completed props basics lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,21 @@
 import React from "react";
+import Links from './Links'
 
-function About() {
+function About(props) {
+  let propsBio
+  if(props.bio === '' || !props.bio){
+    propsBio = false
+  }else{
+    propsBio = true
+  }
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {propsBio && <p>{props.bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+
+      <Links github={<a href="https://github.com/liza">https://github.com/liza</a>} linkedin={<a href="https://www.linkedin.com/in/liza/">https://www.linkedin.com/in/liza/</a>}></Links>
     </div>
   );
 }
---
+++ src/components/Links.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Links(props) {
+  return (
+    <div>
+      <h3>Links</h3>
+        {props.github}
+        {props.linkedin}
+    </div>
+  )
+}
+
+export default Links
